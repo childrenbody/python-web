28a779fe3c590d5f57873ca0a3b50a9f02be9e50
lufei <lufei@wondertechlabs.com>
2024-01-12 18:48:34 +0800

    feat: add antd

---
+++ web/src/app/component/kvs.tsx
@@ -0,0 +1,14 @@
+import {FC} from 'react';
+import {Layout} from 'antd';
+
+
+const kvs: FC = () => {
+    return (
+        <>
+            Hello, World
+        </>
+    )
+}
+
+
+export default kvs
